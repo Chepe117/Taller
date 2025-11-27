37701076d31a8f8986cb82337c9cf2889d6d9da6
Chepe <hjosemanuel169@gmail.com>
2025-11-27 08:12:38 -0600

    Funcion restar

--- Ejemplo.js
+++ Ejemplo.js
@@ -5,6 +5,11 @@ function Sumar(a,b){
 console.log(Sumar(2,4))
 console.log(Sumar(3,4))
 
+//funcion restar
+function Restar(a,b){
+    return a-b
+}
+
 //funcion dividir
 function Dividir(a,b){
     return a/b
